368f74fbb690dc9925fb56e9f35316547972e83b
Nimira43 <n.rabone72@aol.com>
2025-12-03 03:03:35 +0000

    Updated tailwind.config. hover-transition  and button styling created in custom CSS.

--- app/page.js
+++ app/page.js
@@ -1,8 +1,16 @@
 const HomePage = () => {
   return ( 
-    <div className='p-8 bg-green-500'>
-      <h1 className='text-white text-3xl logo'>Longhouse</h1>
-    </div>
+    <>
+      <div className='p-8 bg-green-500'>
+        <h1 className='text-white text-3xl logo'>Longhouse</h1>
+      </div>
+      <div className='m-8'>
+        <button className='m-4 main-btn'>Hello</button>
+        <button className='m-4 main-outline-btn'>Hello</button>
+        <button className='m-4 dark-btn'>Hello</button>
+        <button className='m-4 dark-outline-btn'>Hello</button>
+      </div>
+    </>
   )
 }
  
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,24 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main': '#22c55e',
+        'main-dark': '#15803d',
+        'main-light': '#f0fdf4',
+        'support': '#ffd700',
+        'light': '#f0fdf4',
+        'dark': '#111',
+        'grey-1': '#333',
+        'grey-2': '#999',
+        'grey-3': '#ccc',
+        'grey-4': '#eee',
+        'grey-hover': '#555',
+      },
+      gridTemplateColumns: {
+        '70/30': '70% 28%'
+      },
+    },
   },
   plugins: [],
 }
