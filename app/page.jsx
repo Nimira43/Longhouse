fc7b3e9553d5eec0b274c0c48d7c9d9dd0e27dc6
Nimira43 <n.rabone72@aol.com>
2025-12-03 04:37:32 +0000

    Created PropertiesPage and components/Navbar.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,11 @@
+import Link from 'next/link'
+
 const HomePage = () => {
   return ( 
-    <>
-      <div className='p-8 bg-green-500'>
-        <h1 className='text-white text-3xl logo'>Longhouse</h1>
-      </div>
-      <div className='m-8'>
-        <button className='m-4 main-btn'>Hello</button>
-        <button className='m-4 main-outline-btn'>Hello</button>
-        <button className='m-4 dark-btn'>Hello</button>
-        <button className='m-4 dark-outline-btn'>Hello</button>
-      </div>
-      <div className='m-8'>
-        <p>Longhouse Property Services were established in 1958 by George Longhouse. Today it is one of the UK's most respected property management companies. </p>
-      </div>
-    </>
+    <div>
+      <h1>Longhouse</h1>
+      <Link href='properties'>Properties</Link>
+    </div>
   )
 }
  
