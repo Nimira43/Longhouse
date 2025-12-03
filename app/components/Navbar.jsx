d99ebdfd871d1419b0ba976c93918cefc7148c89
Nimira43 <n.rabone72@aol.com>
2025-12-03 07:30:08 +0000

    Navbar refactor completed

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -8,8 +8,8 @@ const Navbar = () => {
   return ( 
     <nav className='bg-main border-b border-support'>
       <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
-        <div className='relative flex h-20 items-center justify-between'>
-          <div className='absolute inset-y-0 left-0 flex items-center md:hidden'>
+        <div className='relative just-between h-20'>
+          <div className='absolute inset-y-0 left-0 centre-items md:hidden'>
             <button
               type='button'
               id='mobile-dropdown-button'
@@ -19,32 +19,34 @@ const Navbar = () => {
               <TfiMenu />
             </button>
           </div>
-          <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
-            <Link className='flex flex-shrink-0 items-center' href='/'>
+          <div className='flex items-center justify-center flex-1 md:items-stretch md:justify-start'>
+            <Link className='centre-items flex-shrink-0' href='/'>
               <Image
-                className='h-10 w-auto'
+                className='h-9 w-auto'
                 src={logo}
                 alt='Logo'
               />
-              <span className='hidden md:block text-grey-light text-2xl ml-2 logo tracking-wider'>Longhouse</span>
+              <span className='hidden md:block text-dark text-2xl ml-2 logo'>
+                Longhouse
+              </span>
             </Link>
             <div className='hidden md:ml-6 md:block'>
               <div className='flex space-x-2'>
                 <Link
                   href='/'
-                  className='text-light hover:bg-main-dark hover:text-grey-light rounded-md px-3 py-2'
+                  className='nav-link'
                 >
                   Home
                 </Link>
                 <Link
                   href='/properties'
-                  className='text-light hover:bg-main-dark hover:text-grey-light rounded-md px-3 py-2'
+                  className='nav-link'
                 >
                   Properties
                 </Link>
                 <Link
                   href='/properties/add'
-                  className='text-light hover:bg-main-dark hover:text-grey-light rounded-md px-3 py-2'
+                  className='nav-link'
                 >
                   Add Property
                 </Link>
@@ -53,16 +55,17 @@ const Navbar = () => {
           </div>
 
           <div className='hidden md:block md:ml-6'>
-            <div className='flex items-center'>
-              <button
-                className='flex items-center text-light bg-dark hover:bg-main-dark hover:text-grey-light rounded-md px-3 py-2'
-              >
-                <span className='uppercase'>Login or Register</span>
+            <div className='centre-items'>
+              <button className='login-btn'>
+                <span className='uppercase'>Login</span>
               </button>
             </div>
           </div>
-          <div className='absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0'>
-            <Link href='/messages' className='relative group'>
+          <div className='nav-control'>
+            <Link
+              href='/messages'
+              className='relative group'
+            >
               <button
                 type='button'
                 className='notification-btn focus:outline-none'
@@ -90,8 +93,6 @@ const Navbar = () => {
                 id='user-menu'
                 className='user-menu focus: outline-none'
                 role='menu'
-                aria-orientation='vertical'
-                aria-labelledby='user-menu-button'
                 tabIndex='-1'
               >
                 <Link
@@ -130,27 +131,24 @@ const Navbar = () => {
         <div className='space-y-1 px-2 pb-3 pt-2'>
           <Link
             href='/'
-            className='text-light hover:text-grey-light hover:bg-main-dark block rounded-md px-3 py-2 text-base font-medium'
+            className='block nav-link'
           >
             Home
           </Link>
           <Link
             href='/properties'
-            className='text-light hover:text-grey-light hover:bg-main-dark block rounded-md px-3 py-2 text-base font-medium'
+            className='block nav-link'
           >
             Properties
           </Link>
           <Link
             href='/properties/add'
-            className='text-light hover:text-grey-light hover:bg-main-dark block rounded-md px-3 py-2 text-base font-medium'
+            className='block nav-link'
           >
             Add Property
           </Link>
-          <button
-            className='flex items-center text-light bg-dark hover:bg-main-dark hover:text-grey-light rounded-md px-3 py-2 my-5'
-          >
-            <i className='fa-brands fa-google mr-2'></i>
-            <span className='uppercase'>Login or Register</span>
+          <button className='login-btn my-5'>
+            <span className='uppercase'>Login</span>
           </button>
         </div>
       </div>       
