235dc704827b3d766c68ef76ff70e2058b3731ed
Nimira43 <n.rabone72@aol.com>
2026-03-16 07:51:04 +0000

    Docker compose file for database. Setup connection - all good.

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,11 @@
 import HomeProperties from '../components/HomePropertie'
 import Hero from '../components/Hero'
 import InfoBoxes from '../components/InfoBoxes'
+import connectDB from '../config/database'
 
 const HomePage = () => {
+  connectDB()
+
   return ( 
     <div>
       <Hero />
