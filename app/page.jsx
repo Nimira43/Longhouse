50919bbf64ac3df586446aef08a25b574fcef6f8
Nimira43 <n.rabone72@aol.com>
2026-03-22 05:08:02 +0000

    User and Property models

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,8 @@
 import HomeProperties from '../components/HomePropertie'
 import Hero from '../components/Hero'
 import InfoBoxes from '../components/InfoBoxes'
-import connectDB from '../config/database'
 
 const HomePage = () => {
-  connectDB()
 
   return ( 
     <div>
