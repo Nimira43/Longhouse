9f20290e3db846978a37a8d933db185ea55f4bf1
Nimira43 <n.rabone72@aol.com>
2026-04-12 09:54:31 +0100

    PropertiesDetails component done for the moment.

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,12 +1,14 @@
 import Link from 'next/link'
 import PropertyHeaderImage from '../../../components/PropertyHeaderImage'
+import PropertyDetails from '../../../components/PropertiesDetails'
 import connectDB from '../../../config/database'
 import Property from '../../../models/Property'
 import { HiChevronLeft } from 'react-icons/hi'
 
 const PropertyPage = async ({ params }) => {
+  const { id } = await params
   await connectDB()
-  const property = await Property.findById(params.id).lean()
+  const property = await Property.findById(id).lean()
 
   return (
     <>
@@ -22,10 +24,10 @@ const PropertyPage = async ({ params }) => {
           </Link>
         </div>
       </section>
-      <section class='bg-main-light'>
-        <div class='container m-auto py-10 px-6'>
-          <div class='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
-            <div>Placeholder</div>
+      <section className='bg-main-light'>
+        <div className='container m-auto py-10 px-6'>
+          <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
+            <PropertyDetails property={property} />
           </div>
         </div>
       </section>
--- components/PropertiesDetails.jsx
+++ components/PropertiesDetails.jsx
@@ -1,23 +1,22 @@
 import { PiBathtub } from 'react-icons/pi'
-import { RiMoneyPoundBoxLine } from 'react-icons/ri'
 import { RxCross2 } from 'react-icons/rx'
 import { LiaMapMarkerAltSolid, LiaRulerCombinedSolid } from 'react-icons/lia'
 import { MdOutlineKingBed, MdOutlineCheck } from 'react-icons/md'
 
-const PropertiesDetails = () => {
+const PropertyDetails = ({ property }) => {
   return (  
     <main>
       <div className='bg-light p-6 rounded-lg shadow-md text-center md:text-left'>
-        <div className='text-grey-1 mb-4'>
-          Apartment
+        <div className='text-main-dark font-medium uppercase mb-4'>
+          {property.type}
         </div>
         <h1 className='text-3xl font-medium mb-4'>
-          Boston Commons Retreat
+          {property.name}
         </h1>
-        <div className='text-grey-1 mb-4 flex align-middle justify-center md:justify-start'>
-          <LiaMapMarkerAltSolid className='text-lg text-main mr-2' />
-          <p className='text-main'>
-            120 Tremont Street Boston, MA 02111
+        <div className='text-grey-1 mb-4 flex align-middle justify-center md:justify-start text-center'>
+          <LiaMapMarkerAltSolid className='text-lg text-main-dark mt-0.5 mr-2' />
+          <p className='text-main-dark'>
+            {property.location.street}, {property.location.city}, {property.location.postcode}
           </p>
         </div>
 
@@ -26,27 +25,50 @@ const PropertiesDetails = () => {
         </h3>
         <div className='flex flex-col md:flex-row justify-around'>
           <div className='flex items-center justify-center mb-4 border-b border-grey-3 md:border-b-0 pb-4 md:pb-0'>
-            <div className='text-grey-1 mr-2 font-bold'>
+            <div className='text-grey-1 mr-2 font-medium'>
               Nightly
             </div>
-            <div className='text-2xl font-medium'>
-              <RxCross2 className='text-main' />
+            <div className='text-2xl font-medium text-main-dark'>
+              {property.rates.nightly
+                ? (
+                  `£${property.rates.nightly.toLocaleString()}`
+                )
+                : (
+                  <RxCross2 className='text-red-500' />
+                )
+              }
+              
             </div>
           </div>
           <div className='flex items-center justify-center mb-4 border-b border-grey-3 md:border-b-0 pb-4 md:pb-0'>
             <div className='text-grey-1 mr-2 font-medium'>
               Weekly
             </div>
-            <div className='text-2xl font-medium text-main'>
-              £1,100
+            <div className='text-2xl font-medium text-main-dark'>
+              {property.rates.weekly
+                ? (
+                  `£${property.rates.weekly.toLocaleString()}`
+                )
+                : (
+                  <RxCross2 className='text-red-500' />
+                )
+              }
+              
             </div>
           </div>
           <div className='flex items-center justify-center mb-4 pb-4 md:pb-0'>
             <div className='text-grey-1 mr-2 font-medium'>
               Monthly
             </div>
-            <div className='text-2xl font-medium text-main'>
-              £4,200
+            <div className='text-2xl font-medium text-main-dark'>
+              {property.rates.monthly
+                ? (
+                  `£${property.rates.monthly.toLocaleString()}`
+                )
+                : (
+                  <RxCross2 className='text-red-500' />
+                )
+              }
             </div>
           </div>
         </div>
@@ -56,22 +78,22 @@ const PropertiesDetails = () => {
         <h3 className='text-lg font-medium mb-6'>
           Description & Details
         </h3>
-        <div className='flex justify-center gap-4 text-main mb-4 text-xl space-x-9'>
+        <div className='flex justify-center gap-4 text-main-dark mb-4 text-xl space-x-9'>
           <p>
-            <MdOutlineKingBed /> 3
+            <MdOutlineKingBed /> {property.beds}{' '}
             <span className='hidden sm:inline'>Beds</span>
           </p>
           <p>
-            <PiBathtub /> 2
+            <PiBathtub /> {property.baths}{' '}
             <span className='hidden sm:inline'>Baths</span>
           </p>
           <p>
-            <LiaRulerCombinedSolid />
-            1,500 <span className='hidden sm:inline'>sqft</span>
+            <LiaRulerCombinedSolid /> {property.square_feet}{' '}
+            <span className='hidden sm:inline'>sqft</span>
           </p>
         </div>
         <p className='text-grey-1 mb-4'>
-          This is a beautiful apartment located near the commons
+          {property.description}
         </p>
       </div>
 
@@ -81,48 +103,12 @@ const PropertiesDetails = () => {
         </h3>
 
         <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 list-none'>
-          <li>
-            <MdOutlineCheck className='text-main mr-2 mt-3' />Wifi
-          </li>
-          <li>
-            <MdOutlineCheck className='text-main mr-2 mt-3' />Full kitchen
-          </li>
-          <li>
-            <MdOutlineCheck className='text-main mr-2 mt-3' />Washer & Dryer
-          </li>
-          <li>
-            <MdOutlineCheck className='text-main mr-2 mt-3' />Free Parking
-          </li>
-          <li>
-            <MdOutlineCheck className='text-main mr-2 mt-3' />Hot Tub
-          </li>
-          <li>
-            <MdOutlineCheck className='text-main mr-2 mt-3' />24/7 Security
-          </li>
-          <li>
-            <MdOutlineCheck className='text-main mr-2 mt-3' />Wheelchair Accessible
-          </li>
-          <li>
-            <MdOutlineCheck className='text-main mr-2 mt-3' />Lift Access
-          </li>
-          <li>
-            <MdOutlineCheck className='text-main mr-2 mt-3' />Dishwasher
-          </li>
-          <li>
-            <MdOutlineCheck className='text-main mr-2 mt-3' />Gym
-          </li>
-          <li>
-            <MdOutlineCheck className='text-main mr-2 mt-3' />Air Conditioning
-          </li>
-          <li>
-            <MdOutlineCheck className='text-main mr-2 mt-3' />Balcony/Patio
-          </li>
-          <li>
-            <MdOutlineCheck className='text-main mr-2 mt-3' />Smart TV
-          </li>
-          <li>
-            <MdOutlineCheck className='text-main mr-2 mt-3' />Coffee Maker
-          </li>
+          {property.amenities.map((amenity, index) => (
+            <li key={index}>
+              <MdOutlineCheck className='text-main-dark mr-2 mt-3' />{' '}
+              {amenity}
+            </li>
+          ))}
         </ul>
       </div>
       <div className='bg-light p-6 rounded-lg shadow-md mt-6'>
@@ -132,4 +118,4 @@ const PropertiesDetails = () => {
   )
 }
  
-export default PropertiesDetails
+export default PropertyDetails
