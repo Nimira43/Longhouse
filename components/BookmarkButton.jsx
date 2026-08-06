0695a9698ab6f58d59e74a9406d2ddbf70ff060c
Nimira43 <n.rabone72@aol.com>
2026-08-06 18:40:50 +0100

    Check bookmark status.

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -1,6 +1,8 @@
 'use client'
 
+import { useState, useEffect } from 'react'
 import { MdBookmarkBorder } from 'react-icons/md'
+import checkBookmarkStatus from '../app/actions/checkBookmarkStatus'
 import bookmarkProperty from '../app/actions/bookmarkProperty'
 import { toast } from 'react-toastify'
 import { useSession } from 'next-auth/react'
@@ -8,7 +10,22 @@ import { useSession } from 'next-auth/react'
 const BookmarkButton = ({ property }) => {
   const { data: session } = useSession()
   const userId = session?.user?.id
-  
+  const [isBookmarked, setIsBookmarked] = useState(false)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    if (!userId) {
+      setLoading(false)
+      return
+    }
+
+    checkBookmarkStatus(property._id).then((res) => {
+      if (res.error) toast.error(res.error)
+      if (res.isBookmarked) setIsBookmarked(res.isBookmarked)
+      setLoading(false)
+    })
+  }, [property._id, userId, checkBookmarkStatus])
+
   const handleClick = async () => {
     if (!userId) {
       toast.error('You need to be signed in to bookmark a listing.')
@@ -17,11 +34,23 @@ const BookmarkButton = ({ property }) => {
 
     bookmarkProperty(property._id).then((res) => {
       if (res.error) return toast.error(res.error)
+      setIsBookmarked(res.isBookmarked)
       toast.success(res.message)
     })
   }
 
-  return (  
+  if (loading) {
+    return <p className='text-center'>Loading...</p>
+  }
+
+  return isBookmarked ? (  
+    <button
+      className='unbookmark-btn flex items-center justify-center w-full gap-2'
+      onClick={handleClick}
+    >
+      <MdBookmarkBorder /> Remove Bookmark
+    </button>
+  ) : (
     <button
       className='bookmark-btn flex items-center justify-center w-full gap-2'
       onClick={handleClick}
